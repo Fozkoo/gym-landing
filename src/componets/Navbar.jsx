b8b2348a2ad2877a4d579f50c331c4509a896d84
Fozkoo <tmartinelli@alumnos.unsada.edu.ar>
2024-04-03 14:39:58 -0300

    2.3

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import gymLogo from '../images/logo-gym.png';
 import BurgerButton from '..//componets/BurgerButton';
-
+import './navbar.css'
 function Navbar() {
  const [isMenuVisible, setIsMenuVisible] = useState(false);
 
@@ -30,12 +30,39 @@ function Navbar() {
 
             {/* NAV OPTIONS */}
             <div className="contain-options flex gap-7 text-lg font-medium max-lg:hidden">
-              {/* Opciones de navegación */}
+            <div className="contain-options flex gap-7 text-lg font-medium max-lg:hidden">
+                    <a href="#" className="relative group">
+                        Nosotros
+                        <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#FFC900] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                    </a>
+                    <a href="#retos" className="relative group">
+                        Clases
+                        <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#FFC900] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                    </a>
+                    <a href="#ftco-section" className="relative group">
+                        Planes
+                        <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#FFC900] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                    </a>
+                    <a href="#ftco-section" className="relative group">
+                        Beneficios
+                        <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#FFC900] transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
+                    </a>
+                </div>
             </div>
 
             {/* BOTONS OPTIONS */}
             <div className="container-options-slider flex gap-3 justify-center items-center font-semibold text-base max-lg:hidden">
-              {/* Botones de opciones */}
+                <div className="container-options-slider  flex gap-3 justify-center items-center font-semibold text-base max-lg:hidden">
+
+                    <div className='opciones-nav1 cursor-pointer bg-black text-white h-[45px] border-2 justify-center items-center mb-0 border-white px-10 py-[10px] rounded-full hover:bg-white hover:text-black hover:border-black transition-colors duration-500 max-lg:w-[190px] max-lg:h-[45px]'>
+                            <a href="#" className=''>Buscá una clase</a>
+                    </div>
+
+                    <div className='opciones-nav2 cursor-pointer bg-[#FFD300] h-[45px] border-2 border-[#FFD300] px-10 py-[10px] rounded-full hover:bg-black hover:border-white hover:text-white max-lg:border-none  transition-colors duration-500 max-lg:w-[250px] max-lg:h-[45px]'>
+                            <a href="#" className=''>Asociate</a>
+                    </div>
+
+                </div>
             </div>
           </div>
         </div>
@@ -43,7 +70,6 @@ function Navbar() {
       </header>
 
       <div className={`container-menuMobile ${isMenuVisible ? 'flex' : 'hidden'} gap-5 h-56 justify-center items-center bg-black flex-col text-white`}>
-
         <a href="">Nosotros</a>
         <a href="">Contacto</a>
         <a href="">Planes</a>
